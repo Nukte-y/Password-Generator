84b34dac06e84d29ae1796cfe0388e43308e5d01
nukte-y <nukteu@gmail.com>
2024-01-06 18:04:09 +0000

    added function getting inputs from user

--- js/script.js
+++ js/script.js
@@ -90,6 +90,9 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  
+
+  
 
 }
 
@@ -100,7 +103,29 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  do {
+      var length=prompt(`Set a length for your password between 8 to 128 characters`);
+      length=parseInt(length);
+        if (length>=8 && length<=128) {
+          confirm(`Your password will be generated as ${length} characters`);
+          break;
+      } else {
+          alert(`!!!You must enter a valid input between 8 and 128!!!`);
+      }     
+  } while (true);
+  do{
+      alert(`What type of characters do you want in your password:`)
+      var Lowercase=confirm(`lovercase characters?`);
+      var Uppercase=confirm(`Uppercase characters?`);
+      var Numeric=confirm(`Numeric characters?`);
+      var Special=confirm(`Special characters?`);
+        if(Lowercase==true || Uppercase==true || Numeric==true|| Special==true){
+        break;  
+        }
+        else{
+        alert(`!!!You must choose at least one(1) character type!!!`);
+        }  
+  }while(true);  
 }
 
 // Get references to the #generate element
